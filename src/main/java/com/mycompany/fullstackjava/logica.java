4f5becaeb7aeaddf8b60e1793961140b9196b182
MICastilloHurtado <122422130+MICastilloHurtado@users.noreply.github.com>
2023-11-03 14:01:31 -0500

    first commit

---
+++ src/main/java/com/mycompany/fullstackjava/logica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fullstackjava;
+
+/**
+ *
+ * @author marco
+ */
+public class logica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
